1126af1d5f873d535c37f759655bd20d9e0f1d60
will <wbrycejohnson031086@gmail.com>
2022-02-04 17:07:12 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -5,14 +5,24 @@
 /*---------------------------- Variables (state) ----------------------------*/
 
 
+let board, turn, winner
+console.log(board)
+console.log(turn)
+console.log(winner)
+
 
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const stsMsg = document.querySelector('#message')
+// const brd = document.querySelector('.board')
+const sqs = document.querySelectorAll('.game-square')
+// console.log(sqs[1])
+const rplyBtn = document.getElementById('replay')
+// console.log(rplyBtn)
 /*----------------------------- Event Listeners -----------------------------*/
+// tie event listener to .board...
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
 
